2b1714a7ae8d160ac4f1973a32deb3cb24e61883
rchrdwss <richard.weiss@gmx.de>
2020-11-30 09:59:06 +0100

    first list of 3 must contain 1, otherwise there could be a chance not to get 2 other lists of each 3 elements with sum of 15

--- rooms/Charly_das_letzte_Einhorn.py
+++ rooms/Charly_das_letzte_Einhorn.py
@@ -182,7 +182,10 @@ class Charly_das_letzte_Einhorn(EscapeRoom):
         for i in range(riddle["matrix_size"]):
             row = []
             while sum(row) != riddle["magical_sum"]:
-                row = random.sample(all_numbers, riddle["magical_sum"])
+                row = random.sample(all_numbers, 3)
+                if (1 in all_numbers) and (1 not in row):
+                    row = []
+                    continue
             all_numbers = all_numbers.difference(set(row))
             result.append(row)
         return result
